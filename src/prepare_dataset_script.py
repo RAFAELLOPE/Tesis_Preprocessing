4d4578c4293a7d8b8b0d3661fff92a436e348bc8
Rafa <rafaellopezortega87@gmail.com>
2022-12-28 18:08:26 +0100

    Fix problem with paths

--- src/prepare_dataset_script.py
+++ src/prepare_dataset_script.py
@@ -9,23 +9,24 @@ import pandas as pd
 from argparse import ArgumentParser
 import logging
 
-TMP_path = '../data'
+TMP_path = './data'
 
-def create_workflow(df, output_size):
+def create_workflow(df, output_size, org_base_path):
     
     def resample_image(in_file, out_file, out_size):
-        import nibabel as nib 
+        import nibabel.processing
+        import nibabel as nib
         vol = nib.load(in_file)
-        res = nib.processing.conform(vol, out_size)
+        res = nibabel.processing.conform(vol, out_size)
         nib.save(res, out_file)
         return out_file
 
     df_tmp = df.copy()
     df_tmp['tmp_path'] = df_tmp['dst_path'].apply(lambda x: os.path.join(TMP_path, x.split('/')[-1]))
 
-    input_files = [os.path.abspath(p) for p in df_tmp['org_path']]
+    input_files = [os.path.join(org_base_path, p) for p in df_tmp['org_path']]
     resampled_files = [os.path.abspath(p) for p in df_tmp['tmp_path']]
-    result_file = [os.path.abspath(p) for p in df_tmp['dst_path']]
+    result_files = [os.path.abspath(p) for p in df_tmp['dst_path']]
     
     resample = MapNode(Function(input_names=["in_file","out_file", "out_size"],
                              output_names=["res_file"],
@@ -37,33 +38,37 @@ def create_workflow(df, output_size):
     resample.inputs.out_file = resampled_files
     resample.inputs.out_size = output_size
 
-    skullstripping = MapNode(fsl.BET(output_type = 'NIFTI',
-                                    out_file = result_file),
-                            name='skull_strip',
-                            iterfield=['in_file'])
-
+    skullstripping = MapNode(fsl.BET(output_type = 'NIFTI'),
+                                     name='skull_strip',
+                                     iterfield=['in_file', 'out_file'])  
+    skullstripping.inputs.out_file = result_files
+    
     # Workflow initialization
-    wr_base_dir = os.path.abspath('../data/working_dir')
+    wr_base_dir = os.path.abspath('./data/working_dir')
     wf = Workflow(name='Normalization_Workflow', base_dir=wr_base_dir)
 
     # First the simple but more restrictive method
     wf.connect(resample, "res_file", skullstripping, "in_file")
-    return wf
+    return wf, resampled_files
 
 
-def run_preprocessing_workflow(df, output_size):
+def run_preprocessing_workflow(df, output_size, org_base_path):
     # Try-catch + log
     df_res = df.copy()
     num_batches = len(set(df['batch']))
     for i in range(num_batches):
         logging.info(f'Starting batch {i}')
         df_b = df_res[df_res['batch'] == i]
-        wf = create_workflow(df_b, output_size)
+        wf, tmp_files = create_workflow(df_b, output_size, org_base_path)
         try:
             wf.run()
         except:
             logging.error(f'Batch {i} failed')
             continue
+        finally:
+            for f in tmp_files:
+                if os.path.exists(f):
+                    os.remove(f)
 
 
 def define_batches_of_data(df, num_batches):
@@ -100,7 +105,10 @@ def set_train_val_test_split(df):
 
 
 def create_dest_paths(df, dest_base_path):
-    df_tmp = df
+    df_tmp = df.copy()
+    df_tmp['T1'] = df['NewNiftiPath'].apply(lambda x: x.split('/')[4] == 'T1')
+    df_tmp = df_tmp[df_tmp['T1'] == True]
+    df_tmp.drop('T1', axis=1, inplace=True)
     def set_dataset_path(x):
         tmp_path = 'NonPD'
         file = x['org_path'].split('/')[-1]
@@ -111,6 +119,7 @@ def create_dest_paths(df, dest_base_path):
         return dst_path
     
     df_tmp['org_path'] = df_tmp['NewNiftiPath'].apply(lambda x: x.replace('.nii.gz', '.nii'))
+    df_tmp['org_path'] = df_tmp['org_path'].apply(lambda x: x.replace('neuro_db', 'neuro_db_T1'))
     df_res = set_train_val_test_split(df_tmp)
     df_res['dst_path'] = df_res.apply(set_dataset_path, axis=1)
     return df_res
@@ -123,11 +132,6 @@ def manage_arguments():
                         type=str, 
                         required=False,
                         default='')
-    parser.add_argument('--pd_dataset_path', 
-                        help='Path destination dataset', 
-                        type=str, 
-                        required=False,
-                        default='')
     args = parser.parse_args()
     return args
 
@@ -136,13 +140,13 @@ def manage_arguments():
 def main(args):
     # Read input variables 
     DB_base_path = args.db_base_path
-    DB_csv_file = os.path.join(DB_base_path, 'neuro_db_metadata_with_labels.csv')
-    PD_dataset_path = args.pd_dataset_path
     assert os.path.exists(DB_base_path) and DB_base_path != ''
-    assert os.path.exists(DB_csv_file) and DB_csv_file != ''
-    assert os.path.exists(PD_dataset_path) and PD_dataset_path != ''
     # Configure logging
-    logging.basicConfig(filename='../log/results.log',
+    
+    DB_csv_file = os.path.join(DB_base_path, 'neuro_db_T1', 'neuro_db_metadata_with_labels.csv')
+    PD_dataset_path = os.path.join(DB_base_path, 'PD_Dataset')
+
+    logging.basicConfig(filename='./log/results.log',
                         format='%(asctime)s - %(message)s', 
                         datefmt='%d-%b-%y %H:%M:%S', 
                         level=logging.DEBUG)
@@ -151,8 +155,7 @@ def main(args):
     df_tmp = create_dest_paths(df_csv, dest_base_path=PD_dataset_path)
     df_batch = define_batches_of_data(df_tmp, num_batches=9)
     df_batch.to_csv(os.path.join(PD_dataset_path, 'data_index.csv'), index=False)
-    run_preprocessing_workflow(df_batch, output_size=(181, 217, 181))
-
+    run_preprocessing_workflow(df_batch, output_size=(181, 217, 181), org_base_path=DB_base_path)
 
 
 if __name__ == '__main__':
